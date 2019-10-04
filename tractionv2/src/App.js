ff358e741ead56ec46985c1c54e21a2fed873a36
dcz-ii <danilo@mytractiontools.com>
2019-10-04 13:13:05 +0800

    Add todo from hasura.

--- tractionv2/src/App.js
+++ tractionv2/src/App.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { useSubscription } from "react-apollo-hooks";
+import { useQuery } from "react-apollo-hooks";
 import gql from "graphql-tag";
+import _ from 'lodash';
 
 import './App.css';
 import Home from './pages/Home';
@@ -12,28 +13,50 @@ function App(props) {
     props.getData();
   }, [])
 
-  const NEW_USER_SUB = gql`subscription {
-    newUser {
-      id
-      username
-      firstLetterOfUsername
-      todo {
-        task
-        completed
+  const GET_MY_TODOS = gql`
+    query getTodos {
+      todos {
+        id
+        is_completed
+        created_at
+        title
+        user {
+          name
+        }
       }
     }
-  }`
+    `;
 
-  const { data, loading } = useSubscription(
-    NEW_USER_SUB
-  );
+  const { data, error, loading } = useQuery(GET_MY_TODOS);
+
 
   console.log(loading)
 
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+  
+  console.log(data)
   return (
     <div className="App">
-     <Home />
-      {loading ? '...' :  data}
+      <table>
+        <tr>
+          <th>Title</th>
+          <th>Name</th>
+          <th>completed</th>
+          <th>created</th>
+        </tr>
+        {_.map(data.todos, (data, idx) => {
+          return(
+            <tr>
+              <td width="25%">{data.title}</td>
+              <td width="25%">{data.user.name}</td>
+              <td width="25%">{data.is_completed}</td>
+              <td width="25%">{data.created_at}</td>
+            </tr>
+          )
+        })}
+
+      </table>
     </div>
   );
 }
--- tractionv2/src/index.js
+++ tractionv2/src/index.js
@@ -5,13 +5,12 @@ import { createStore, applyMiddleware } from 'redux';
 import { persistReducer } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
 import storage from 'redux-persist/lib/storage';
-import ApolloClient from "apollo-boost";
+// import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 import { ApolloProvider as ApolloHooksProvider } from "react-apollo-hooks";
-import { WebSocketLink } from "apollo-link-ws";
-import { InMemoryCache, split } from "apollo-boost";
-import { getMainDefinition } from "apollo-utilities";
-import { createHttpLink } from "apollo-link-http";
+import ApolloClient from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
 
 import './index.css';
 import App from './App';
@@ -29,31 +28,19 @@ const persistedRecuder = persistReducer(persistConfig, reducers);
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(persistedRecuder, applyMiddleware(sagaMiddleware));
 
-const httpLink = createHttpLink({
-	uri: 'http://localhost:4000/graphql',
-	credentials: "include"
+const createApolloClient = (authToken) => {
+  return new ApolloClient({
+    link: new HttpLink({
+      uri: 'https://learn.hasura.io/graphql',
+      headers: {
+        Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik9FWTJSVGM1UlVOR05qSXhSRUV5TURJNFFUWXdNekZETWtReU1EQXdSVUV4UVVRM05EazFNQSJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWRlZmF1bHQtcm9sZSI6InVzZXIiLCJ4LWhhc3VyYS1hbGxvd2VkLXJvbGVzIjpbInVzZXIiXSwieC1oYXN1cmEtdXNlci1pZCI6ImF1dGgwfDVkOTZjN2FiZmJmZDllMGRmMWRhOTNhNiJ9LCJuaWNrbmFtZSI6ImRhbmlsbyIsIm5hbWUiOiJkYW5pbG9AbXl0cmFjdGlvbnRvb2xzLmNvbSIsInBpY3R1cmUiOiJodHRwczovL3MuZ3JhdmF0YXIuY29tL2F2YXRhci8wNTU5N2RkNjI4MDM1YWZjOTJmNzM1ZGQ2NmIyMmQ5YT9zPTQ4MCZyPXBnJmQ9aHR0cHMlM0ElMkYlMkZjZG4uYXV0aDAuY29tJTJGYXZhdGFycyUyRmRhLnBuZyIsInVwZGF0ZWRfYXQiOiIyMDE5LTEwLTA0VDA0OjE2OjQ0LjMxNFoiLCJpc3MiOiJodHRwczovL2dyYXBocWwtdHV0b3JpYWxzLmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw1ZDk2YzdhYmZiZmQ5ZTBkZjFkYTkzYTYiLCJhdWQiOiJQMzhxbkZvMWxGQVFKcnprdW4tLXdFenFsalZOR2NXVyIsImlhdCI6MTU3MDE2MjYwNCwiZXhwIjoxNTcwMTk4NjA0LCJhdF9oYXNoIjoiWUNlVVFLUi05ODc5dHNpdFJka3p1USIsIm5vbmNlIjoiQy1WNzF5cVF5cUxETmdETnRZb1RublVfbVZKNDlsSlEifQ.ApS8x0mD3aLa0lAP9crI8MGDOynwR-4x8b1iwbcytn3r2PD2YDbyieyp39k7oHVyRQ-8O6QFOfw8o5efPibdKysiFqj6VihXKXdJXSiGg-EH23syvQuFPS6Kv_mWUeXludIhZtPTego7RQKiSh192N0C5l0EQqQzR5l9bL4OG9QX59WdpF4hW5_bggMNgGxc20XiqaDlj09zjhv-vd-5a0prh8regFaeqSOmqva7jcftZE6S3lyZlVCuSk4b1145HCUyPo4iuIAhk8eltQgvi1Ker6H5XhashH6258-Ue0H9D5ffI0guN07-Sl6-Fh-3vuvILxO4kYBaBHyaSvDolg'
+      }
+    }),
+    cache: new InMemoryCache(),
   });
+ };
 
-const wsLink = new WebSocketLink({
-  uri: `ws://localhost:4000/`,
-  options: {
-	reconnect: true
-  }
-});
-
-const link = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return (
-      definition.kind === "OperationDefinition" &&
-      definition.operation === "subscription"
-    );
-  },
-  wsLink,
-  httpLink
-);
-
-const client = new ApolloClient({ link, cache: new InMemoryCache() });
+ const client = createApolloClient();
 
 ReactDOM.render(
 	<Provider store={store}>
