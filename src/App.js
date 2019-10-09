9fa92ce2870a5da922d2383193b7dddfb0a301d0
dcz-ii <danilo@mytractiontools.com>
2019-10-09 19:14:15 +0800

    Add delete functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useMutation, useSubscription } from "react-apollo-hooks";
 import gql from "graphql-tag";
 import moment from 'moment';
 import _ from 'lodash';
 
 import './App.css';
-import { DirectiveLocation } from 'graphql';
+// import { DirectiveLocation } from 'graphql';
 
 function App(props) {
   const [todoTitle, handleTitle] = useState('');
@@ -36,7 +36,16 @@ function App(props) {
     }
   `;
 
+  const DELETE_TODO = gql`
+    mutation removeTodo ($id: Int!) {
+      delete_todos(where: {id: {_eq: $id}}) {
+        affected_rows
+      }
+    }
+  `
+
   let { data, error, loading } = useSubscription(GET_MY_TODOS);
+
   let [toggleAddToDo, { loadingAdd }] = useMutation(ADD_TODO, {
     update: (proxy, mutationResult) => {
       /* your custom update logic */
@@ -45,7 +54,9 @@ function App(props) {
     variables: { todoTitle }
   });
 
-  if (loading || loadingAdd) return 'Loading...';
+  const [toggleDelete, { loadingDel }] = useMutation(DELETE_TODO);
+
+  if (loading || loadingAdd || loadingDel) return 'Loading...';
   if (error) return `Error! ${error.message}`;
   
   console.log(data)
@@ -54,17 +65,27 @@ function App(props) {
       <table>
         <tr>
           <th>Title</th>
+          <th>ID</th>
           <th>Name</th>
           <th>completed</th>
           <th>created</th>
+          <th>delete?</th>
         </tr>
         {_.map(data.todos, (data, idx) => {
           return(
             <tr key={idx}>
               <td width="25%">{data.title}</td>
+              <td width="25%">{data.id}</td>
               <td width="25%">{data.user.name}</td>
-              <td width="25%">{data.is_completed}</td>
+              <td width="25%">{_.toString(data.is_completed)}</td>
               <td width="25%">{moment(data.created_at).format('MMMM Do, h:mm')}</td>
+              <td width="25%">
+                {data.user.name === 'danilo' && <button onClick={() => {
+                  toggleDelete({ variables: { id: data.id } });
+                }}>
+                  delete
+                </button>}
+              </td>
             </tr>
           )
         })}
