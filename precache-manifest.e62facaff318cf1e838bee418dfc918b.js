f3bcc6962ee402b7ac90554f1219650a9be74f37
dcz-ii <danilo@mytractiontools.com>
2019-10-24 10:29:47 +0800

    Updates

---
+++ precache-manifest.e62facaff318cf1e838bee418dfc918b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ee39f2a0b1d4a51abc4c7425ab33d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8887be0031b7c7cbbdd",
+    "url": "/static/css/main.3de57f41.chunk.css"
+  },
+  {
+    "revision": "107e8b9db8626671117f",
+    "url": "/static/js/2.a5021819.chunk.js"
+  },
+  {
+    "revision": "a8887be0031b7c7cbbdd",
+    "url": "/static/js/main.b36a1b85.chunk.js"
+  },
+  {
+    "revision": "bfa040be50a191489baf",
+    "url": "/static/js/runtime-main.5a79945f.js"
+  }
+]);
